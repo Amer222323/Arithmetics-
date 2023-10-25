2e18cf97b0f46ac40d360b42a8647d42c94a75b7
Amer Al-Aloush <inf4049@hs-worms.de>
2023-10-25 14:25:56 +0200

    new Commit

--- src/Arthmitics.java
+++ src/Arthmitics.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Arthmitics {
+public class Arthmitics {
+    public int  sum(int x,int y){
+        return x+y;
+    }
+    public int sub(int x,int y){
+        return x-y;
+    }
+    public int mult(int x,int y){
+        return x*y;
+    }
+    public int div(int x,int y){
+        return x/y;
+    }
+    public int modulo(int x, int y){
+        return x%y;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n1, n2;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        n1 = scanner.nextInt();
+
+        System.out.println("Enter the second number: ");
+        n2 = scanner.nextInt();
+
+        System.out.println("Enter the name of Operation (sum, mult, div, modulo, sub):");
+        String operation = scanner.next();
+
+        int answer = 0;
+        Arthmitics calculator = new Arthmitics();
+
+        if (operation.equals("sum")) {
+            answer = calculator.sum(n1, n2);
+        } else if (operation.equals("mult")) {
+            answer = calculator.mult(n1, n2);
+        } else if (operation.equals("div")) {
+            if (n2==0){
+                System.out.println("Answer is Undefined");
+                System.exit(1);
+            }else {
+            answer = calculator.div(n1, n2);}
+        } else if (operation.equals("modulo")) {
+            answer = calculator.modulo(n1, n2);
+        } else if (operation.equals("sub")) {
+            answer = calculator.sub(n1, n2);
+        } else {
+            System.out.println("Invalid operation.");
+            System.exit(1);
+        }
+
+        System.out.println("The Answer: " + answer);
     }
+    /*switch (operation) {
+        case "sum" -> answer = calculator.sum(n1, n2);
+        case "mult" -> answer = calculator.mult(n1, n2);
+        case "div" -> answer = calculator.div(n1, n2);
+        case "modulo" -> answer = calculator.modulo(n1, n2);
+        case "sub" -> answer = calculator.sub(n1, n2);
+        default -> {
+            System.out.println("Invalid operation.");
+            System.exit(1);
+        }
+    }
+
+        System.out.println("The Answer: " + answer);
+}*/
 }
